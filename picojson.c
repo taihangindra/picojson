afa8b4256d50e5c3e23a6e8e6ad612d286d89530
zhangyu <jujubefarmer@mail.nwpu.edu.cn>
2023-10-15 21:42:17 +0800

    parse string

--- picojson.c
+++ picojson.c
@@ -2,14 +2,42 @@
 #include <assert.h>  /* assert() */
 #include <stdlib.h>  /* NULL */
 #include <math.h>
+#include <string.h>
+
+#ifndef PJSON_STACK_INIT_SIZE
+#define PJSON_STACK_INIT_SIZE 256
+#endif
 
 #define EXPECT(c, ch)       do { assert(*c -> json == (ch)); c -> json++; } while(0)
 #define ISDIGIT(ch)         ((ch) >= '0' && (ch) <= '9')
 #define ISDIGIT1TO9(ch)     ((ch) >= '1' && (ch) <= '9')
+#define PUTC(c, ch)         do { *(char *)pjson_context_push(c, sizeof(char)) = (ch); } while(0)
 
 typedef struct {
     const char* json;
+    char* stack;
+    size_t size, top;
 }pjson_context;
+
+static void* pjson_context_push(pjson_context* context, size_t size) {
+    void* ret;
+    assert(size > 0);
+    if(context -> top + size >= context -> size) {
+        if(context -> size == 0) context -> size = PJSON_STACK_INIT_SIZE;
+        while (context -> top + size >= context -> size) {
+            context -> size += context -> size >> 1;
+        }
+        context -> stack = (char *)realloc(context -> stack, context -> size);
+    }
+    ret = context -> stack + context -> top;
+    context -> top += size;
+    return ret;
+}
+
+static void* pjson_context_pop(pjson_context* context, size_t size) {
+    assert(context -> top >= size);
+    return context -> stack + (context -> top -= size);
+}
 
 static void pjson_parse_whitespace(pjson_context* c) {
     const char *p = c -> json;
@@ -18,30 +46,14 @@ static void pjson_parse_whitespace(pjson_context* c) {
     c -> json = p;
 }
 
-static int pjson_parse_true(pjson_context* c, pjson_value* v) {
-    EXPECT(c, 't');
-    if (c -> json[0] != 'r' || c -> json[1] != 'u' || c -> json[2] != 'e')
-        return PJSON_PARSE_INVALID_VALUE;
-    c -> json += 3;
-    v -> type = PJSON_TRUE;
-    return PJSON_PARSE_OK;
-}
-
-static int pjson_parse_false(pjson_context* c, pjson_value* v) {
-    EXPECT(c, 'f');
-    if (c -> json[0] != 'a' || c -> json[1] != 'l' || c -> json[2] != 's' || c -> json[3] != 'e')
-        return PJSON_PARSE_INVALID_VALUE;
-    c -> json += 4;
-    v -> type = PJSON_FALSE;
-    return PJSON_PARSE_OK;
-}
-
-static int pjson_parse_null(pjson_context* c, pjson_value* v) {
-    EXPECT(c, 'n');
-    if (c -> json[0] != 'u' || c -> json[1] != 'l' || c -> json[2] != 'l')
-        return PJSON_PARSE_INVALID_VALUE;
-    c -> json += 3;
-    v -> type = PJSON_NULL;
+static int pjson_parse_literal(pjson_context *context, pjson_value* value, const char* literal, pjson_type type) {
+    size_t i;
+    EXPECT(context, literal[0]);
+    for(i = 0; literal[i + 1]; i++) {
+        if(context -> json[i] != literal[i + 1]) return PJSON_PARSE_INVALID_VALUE;
+    }
+    context -> json += i;
+    value -> type = type;
     return PJSON_PARSE_OK;
 }
 
@@ -65,37 +77,72 @@ static int pjson_parse_number(pjson_context* c, pjson_value* v) {
         for (p++; ISDIGIT(*p); p++);
     }
     errno = 0;
-    v->number = strtod(c->json, NULL);
-    if (errno == ERANGE && (v->number == HUGE_VAL || v->number == -HUGE_VAL))
+    v->u.number = strtod(c->json, NULL);
+    if (errno == ERANGE && (v->u.number == HUGE_VAL || v->u.number == -HUGE_VAL))
         return PJSON_PARSE_NUMBER_TOO_BIG;
     v->type = PJSON_NUMBER;
     c->json = p;
     return PJSON_PARSE_OK;
 }
 
-static int pjson_parse_value(pjson_context* c, pjson_value* v) {
-    switch (*c -> json) {
-        case 't':  return pjson_parse_true(c, v);
-        case 'f':  return pjson_parse_false(c, v);
-        case 'n':  return pjson_parse_null(c, v);
-        default:   return pjson_parse_number(c, v);
+static int pjson_parse_string(pjson_context* context, pjson_value* value) {
+    size_t head = context -> top, len;
+    const char* p;
+    EXPECT(context, '\"');
+    p = context -> json;
+    for(;;) {
+        char ch = *p++;
+        switch (ch) {
+            case '\"':
+                len = context -> top - head;
+                pjson_set_string(value, (const char*)pjson_context_pop(context, len), len);
+                context -> json = p;
+                return PJSON_PARSE_OK;
+            case '\0':
+                context -> top = head;
+                return PJSON_PARSE_MISS_QUOTATION_MARK;
+            default:
+                PUTC(context, ch);
+        }
+    }
+
+}
+
+static int pjson_parse_value(pjson_context* context, pjson_value* value) {
+    switch (*context -> json) {
+        case 't':  return pjson_parse_literal(context, value, "true", PJSON_TRUE);
+        case 'f':  return pjson_parse_literal(context, value, "false", PJSON_FALSE);
+        case 'n':  return pjson_parse_literal(context, value, "null", PJSON_NULL);
+        default:   return pjson_parse_number(context, value);
+        case '\"': return pjson_parse_string(context, value);
         case '\0': return PJSON_PARSE_EXPECT_VALUE;
     }
 }
 
-int pjson_parse(pjson_value* v, const char* json) {
-    pjson_context c;
+int pjson_parse(pjson_value* value, const char* json) {
+    pjson_context context;
     int ret;
-    assert(v != NULL);
-    c.json = json;
-    v -> type = PJSON_NULL;
-    pjson_parse_whitespace(&c);
-    if ((ret = pjson_parse_value(&c, v)) == PJSON_PARSE_OK) {
-        pjson_parse_whitespace(&c);
-        if (*c.json != '\0')
+    assert(value != NULL);
+    context.json = json;
+    context.stack = NULL;
+    context.size = 0;
+    pjson_init(value);
+    pjson_parse_whitespace(&context);
+    if ((ret = pjson_parse_value(&context, value)) == PJSON_PARSE_OK) {
+        pjson_parse_whitespace(&context);
+        if (*context.json != '\0')
             ret = PJSON_PARSE_ROOT_NOT_SINGULAR;
     }
+    assert(context.top = 0);
+    free(context.stack);
     return ret;
+}
+
+void pjson_free(pjson_value * value) {
+    assert(value != NULL);
+    if (value->type == PJSON_STRING)
+        free(value->u.string.s);
+    value -> type = PJSON_NULL;
 }
 
 pjson_type pjson_get_type(const pjson_value* json) {
@@ -103,7 +150,40 @@ pjson_type pjson_get_type(const pjson_value* json) {
     return json -> type;
 }
 
-double pjson_get_number(const pjson_value* json) {
-    assert(json != NULL);
-    return json -> number;
+int pjson_get_boolean(const pjson_value* v) {
+    /* \TODO */
+    return 0;
+}
+
+void pjson_set_boolean(pjson_value* v, int b) {
+    /* \TODO */
+}
+
+double pjson_get_number(const pjson_value* v) {
+    assert(v != NULL && v->type == PJSON_NUMBER);
+    return v->u.number;
+}
+
+void pjson_set_number(pjson_value* v, double n) {
+    /* \TODO */
+}
+
+const char* pjson_get_string(const pjson_value* v) {
+    assert(v != NULL && v->type == PJSON_STRING);
+    return v->u.string.s;
+}
+
+size_t pjson_get_string_length(const pjson_value* v) {
+    assert(v != NULL && v->type == PJSON_STRING);
+    return v->u.string.len;
+}
+
+void pjson_set_string(pjson_value* v, const char* s, size_t len) {
+    assert(v != NULL && (s != NULL || len == 0));
+    pjson_free(v);
+    v->u.string.s = (char*)malloc(len + 1);
+    memcpy(v->u.string.s, s, len);
+    v->u.string.s[len] = '\0';
+    v->u.string.len = len;
+    v->type = PJSON_STRING;
 }
--- picojson.h
+++ picojson.h
@@ -5,6 +5,8 @@
 #ifndef PICOJSON_PICOJSON_H
 #define PICOJSON_PICOJSON_H
 
+#include <stddef.h> /* using size_t */
+
 typedef enum{
     PJSON_NULL = 0,
     PJSON_FALSE,
@@ -17,7 +19,10 @@ typedef enum{
 
 typedef struct {
     pjson_type type;
-    double number;
+    union {
+       double number;
+       struct {char* s; size_t len; } string;
+    } u;
 }pjson_value;
 
 enum {
@@ -25,13 +30,28 @@ enum {
     PJSON_PARSE_EXPECT_VALUE,
     PJSON_PARSE_INVALID_VALUE,
     PJSON_PARSE_ROOT_NOT_SINGULAR,
-    PJSON_PARSE_NUMBER_TOO_BIG
+    PJSON_PARSE_NUMBER_TOO_BIG,
+    PJSON_PARSE_MISS_QUOTATION_MARK
 };
+
+#define pjson_init(v) do { (v) -> type = PJSON_NULL; } while(0)
 
 int pjson_parse(pjson_value* value, const char* json);
 
+void pjson_free(pjson_value *value);
+
 pjson_type pjson_get_type(const pjson_value* json);
 
+#define pjson_set_null(v) do { pjson_free(v); } while(0)
+
+int pjson_get_boolean(const pjson_value *value);
+void pjson_set_boolean(pjson_value *value, int boolean);
+
 double pjson_get_number(const pjson_value* json);
+void pjson_set_number(pjson_value *value, double number);
+
+const char *pjson_get_string(const pjson_value *value);
+size_t pjson_get_string_length(const pjson_value *value);
+void pjson_set_string(pjson_value *value, const char* string, size_t length);
 
 #endif //PICOJSON_PICOJSON_H
